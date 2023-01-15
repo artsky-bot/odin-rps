917344ee3d600b506e7b9f8cf44965c65ce19ca8
¨artsky-bot¨ <artsky.task@gmail.com>
2023-01-15 02:37:05 +0000

    Fixed formatting of the game result to output using a paragraph tag. Basic functionality of the rps game completed.

--- script.js
+++ script.js
@@ -3,6 +3,28 @@
 const rockBtn = document.querySelector("#rock-button");
 const paperBtn = document.querySelector("#paper-button");
 const scissorBtn = document.querySelector("#scissor-button");
+const outputResultContainer = document.querySelector("#result-output");
+const outputRoundContainer = document.querySelector("#round-output");
+let outputResultText = document.createElement('p');
+let outputRoundText;
+
+let round = 0;
+let winCondition; 
+let playerPoints = 0; 
+let computerPoints = 0; 
+
+rockBtn.addEventListener('click', () => {
+  rpsGame(0);
+});
+
+paperBtn.addEventListener('click', () => {
+  rpsGame(1);
+});
+
+scissorBtn.addEventListener('click', () => {
+  rpsGame(2);
+});
+
 
 // 0 = rock, 1 = paper, 2 = scissors
 const rpsNumbs = [`rock`, `paper`, `scissors`];
@@ -22,19 +44,22 @@ const getComputerResult = function () {
 // return 0 if its a tie, 1 if the player won the round, 2 if the computer won the round.
 const playRound = function (getPlayerResult, getComputerResult) {
   if (rpsArray[getPlayerResult][getComputerResult] === 0) {
-    console.log(
-      `It's a tie! ${rpsNumbs[getPlayerResult]} doesn't beat ${rpsNumbs[getComputerResult]}!`
-    );
+    outputRoundText = document.createElement('p');
+    outputRoundText.setAttribute('style', 'color: yellow;');
+    outputRoundText.textContent = `It's a tie! ${rpsNumbs[getPlayerResult]} doesn't beat ${rpsNumbs[getComputerResult]}!`;
+    outputRoundContainer.appendChild(outputRoundText);
     return rpsArray[getPlayerResult][getComputerResult];
   } else if (rpsArray[getPlayerResult][getComputerResult] === 1) {
-    console.log(
-      `You won this round! ${rpsNumbs[getPlayerResult]} beats ${rpsNumbs[getComputerResult]}!`
-    );
+    outputRoundText = document.createElement('p');
+    outputRoundText.setAttribute('style', 'color: green;');
+    outputRoundText.textContent = `You won this round! ${rpsNumbs[getPlayerResult]} beats ${rpsNumbs[getComputerResult]}!`;
+    outputRoundContainer.appendChild(outputRoundText);
     return rpsArray[getPlayerResult][getComputerResult];
   } else {
-    console.log(
-      `You lost this round! ${rpsNumbs[getComputerResult]} beats ${rpsNumbs[getPlayerResult]}!`
-    );
+    outputRoundText = document.createElement('p');
+    outputRoundText.setAttribute('style', 'color: red;');
+    outputRoundText.textContent = `You lost this round! ${rpsNumbs[getComputerResult]} beats ${rpsNumbs[getPlayerResult]}!`;
+    outputRoundContainer.appendChild(outputRoundText);
     return rpsArray[getPlayerResult][getComputerResult];
   }
 };
@@ -53,36 +78,19 @@ const rpsGame = function (buttonVal) {
     round++;
     if (round == 5) {
       if (playerPoints > computerPoints) {
-        console.log(
-          `You won this game! Your score is ${playerPoints}:${computerPoints}`
-        );
+        outputResultText.setAttribute('style', 'color: green;');
+        outputResultText.textContent = `You won this game! Your score is ${playerPoints}:${computerPoints}`;
+        outputResultContainer.appendChild(outputResultText);
       } else if (playerPoints === computerPoints) {
-        console.log(
-          `You tied this game! Your score is ${playerPoints}:${computerPoints}`
-        );
+        outputResultText.setAttribute('style', 'color: yellow;');
+        outputResultText.textContent = `You tied this game! Your score is ${playerPoints}:${computerPoints}`;
+        outputResultContainer.appendChild(outputResultText);
       } else {
-        console.log(
-          `You lost this game! Your score is ${playerPoints}:${computerPoints}`
-        );
+        outputResultText.setAttribute('style', 'color: red;');
+        outputResultText.textContent = `You lost this game! Your score is ${playerPoints}:${computerPoints}`;
+        outputResultContainer.appendChild(outputResultText);
       }
     }
   }
 };
-
-let round = 0;
-let winCondition; 
-let playerPoints = 0; 
-let computerPoints = 0; 
-
-rockBtn.addEventListener('click', () => {
-  rpsGame(0);
-});
-
-paperBtn.addEventListener('click', () => {
-  rpsGame(1);
-});
-
-scissorBtn.addEventListener('click', () => {
-  rpsGame(2);
-});
 
